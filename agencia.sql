d78f58d0b95f0d9f1e835155faa9797b9dbd892b
Jacque555 <guadalupehe12@gmail.com>
2022-10-09 10:05:42 -0500

    first commit

---
+++ agencia.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE agencia_automotriz;
+USE agencia_automotriz;
+
+CREATE TABLE producto(
+codigobarras VARCHAR(100) PRIMARY KEY,
+nombre VARCHAR(100),
+descripcion VARCHAR(200),
+marca VARCHAR(100));
+
+CREATE TABLE herramienta(
+codigoherramienta VARCHAR(100) PRIMARY KEY,
+nombre VARCHAR(100),
+medida DOUBLE,
+marca VARCHAR(100),
+descripcion VARCHAR(200));
+
+CREATE TABLE permisos(
+idpermisos ENUM('ADMIN','GENERAL') PRIMARY KEY);
+
+CREATE TABLE usuario(
+idusuario INT PRIMARY KEY AUTO_INCREMENT,
+rfc VARCHAR(50),
+nombre VARCHAR(50),
+apellidop VARCHAR(50),
+apellidom VARCHAR(50),
+fdn DATE,
+fkidpermisos ENUM('ADMIN','GENERAL'),
+FOREIGN KEY(fkidpermisos) REFERENCES permisos(idpermisos));
